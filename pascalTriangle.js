00872caa6ff7c85ce3d96b59ed007006ee09b648
Akshit Thakur <akshit2649@gmail.com>
2022-11-10 20:24:26 +0530

    pascal triangle

---
+++ pascalTriangle.js
@@ -0,0 +1,54 @@
+const initApp = () => {
+  const pascalTriangleArray = pascalTriangle(4);
+  console.log(pascalTriangleArray);
+  domTriangle(pascalTriangleArray);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+const pascalTriangle = (rows = 3) => {
+  if (rows < 3) rows = 3;
+
+  const stackArray = [];
+  let i = 1;
+  while (i <= rows) {
+    const rowArray = [];
+    let x = 0;
+    let rowValue;
+    while (x < i) {
+      if (!x || x === i - 1) {
+        rowValue = 1;
+      } else {
+        rowValue =
+          parseInt(stackArray[i - 2][x]) + parseInt(stackArray[i - 2][x - 1]);
+      }
+      rowArray.push(rowValue);
+      x++;
+    }
+    stackArray.push(rowArray);
+    i++;
+  }
+
+  return stackArray;
+};
+
+const domTriangle = (array) => {
+  const main = document.querySelector("main");
+
+  array.forEach((subArr) => {
+    const row = buildRow(subArr);
+    main.appendChild(row);
+  });
+};
+
+const buildRow = (array) => {
+  const row = document.createElement("div");
+  row.classList.add("row");
+  array.forEach((el) => {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.textContent = el;
+    row.appendChild(square);
+  });
+  return row;
+};
